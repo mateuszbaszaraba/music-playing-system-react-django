0f719561e60e3b63711cdeedd850f28711717518
mateuszbaszaraba <64064788+mateuszbaszaraba@users.noreply.github.com>
2021-11-20 17:09:02 +0100

    updated if statements

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -21,7 +21,7 @@ class GetRoom(APIView):
     code = request.GET.get(self.lookup_url_kwarg)
     if code != None:
       room = Room.objects.filter(code=code)
-      if len(room) > 0:
+      if room.exists():
         data = RoomSerializer(room[0]).data
         data['is_host'] = self.request.session.session_key == room[0].host
         return Response(data, status=status.HTTP_200_OK)
@@ -38,7 +38,7 @@ class JoinRoom(APIView):
     code = request.data.get(self.lookup)
     if code != None:
       room_result = Room.objects.filter(code=code)
-      if len(room_result) > 0:
+      if room_result.exists():
         room = room_result[0]
         self.request.session['room_code'] = code
         return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
@@ -89,7 +89,7 @@ class LeaveRoom(APIView):
       self.request.session.pop('room_code')
       host_id = self.request.session.session_key
       room_results = Room.objects.filter(host=host_id)
-      if len(room_results) > 0:
+      if room_results.exists() > 0:
         room = room_results[0]
         room.delete()
     return Response({'Message': 'Success'}, status=status.HTTP_200_OK)

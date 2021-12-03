6d1191f8c9ffa90e46916040e1e9b6691459c741
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2021-12-03 19:24:09 +0100

    deleted unnecessary comments

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -98,7 +98,7 @@ class UpdateRoom(APIView):
   serializer_class = UpdateRoomSerializer
 
   def patch(self, request, format=None):
-    if not self.request.session.exists(self.request.session.session_key): #czy mozna zapisać krócej self.request.session.session_key.exists()
+    if not self.request.session.exists(self.request.session.session_key):
       self.request.session.create()
 
     serializer = self.serializer_class(data=request.data)
